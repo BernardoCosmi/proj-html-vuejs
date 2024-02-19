9e5e2d0b3b2e17ebf7d4983719e5193e69df55fa
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-19 11:50:00 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ExampleName from "./components/ExampleName.vue"
+  export default{
+    components: {
+      ExampleName
+    }
+  }
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
