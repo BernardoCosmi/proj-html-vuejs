1eeafd05858390a0c3ed68487ce45ce7617943d7
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-21 15:02:40 +0100

    desktop layout complete (from 1440p)

--- src/components/AppMain/AppMainSection/SectionFive.vue
+++ src/components/AppMain/AppMainSection/SectionFive.vue
@@ -100,46 +100,31 @@ figure{
   text-align: center;
   overflow: visible;
   padding: 5px;
+  min-height: 500px;
+  min-width: 600px;
   img{
     scale: 0.9;
   }
 }
 .figure-top {
   position: relative;
-  height: auto; /* Imposta l'altezza in base al contenuto */
-}
-.figure-top::before {
-  content: "";
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 65%;
-  height: 80%;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' width='200px' height='202px' fill='%23ece8e4' viewBox='0 0 200 202'%3E%3Cpath class='elementor-shape-fill' d='M139.374493,198.087515 C155.702187,206.99353 190.724846,197.001323 197.677982,159.049528 C204.827153,120.027728 184.496324,118.997566 188.045232,104.801934 C202.287755,83.6528313 204.827153,56.662651 190.073179,40.4146034 C153.570125,2.39058926 144.444362,48.0194062 114.025151,20.1351292 C101.727304,7.83728238 83.0989526,-6.73517414 56.2286492,3.40456297 C18.2385839,17.7404371 27.3303985,47.659333 27.3303985,69.3128542 C27.3303985,76.9370868 3.11408002,91.2696589 0.460095291,110.885776 C-2.88628833,135.619549 12.6277797,168.175291 27.3303985,179.329002 C50.6517938,205.692318 86.6478605,199.608476 102.87144,190.482713 C116.802125,182.646702 128.220783,192.003673 139.374493,198.087515 Z'%3E%3C/path%3E%3C/svg%3E");
-  z-index: -1;
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-}
-.figure-top::after {
-  content: "";
-  position: absolute;
-  bottom: 25%;
-  right: 0;
-  width: 40%;
-  height: 80%;
-  background-image: url('../../../assets/images/maxcoach-shape-05.png');
-  background-size: 55%;
-  background-repeat: no-repeat;
-  background-position: bottom;
+  overflow: visible; 
+  background-image: url('../../../assets/images/maxcoach-shape-05.png'), url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' width='200px' height='202px' fill='%23ece8e4' viewBox='0 0 200 202'%3E%3Cpath class='elementor-shape-fill' d='M139.374493,198.087515 C155.702187,206.99353 190.724846,197.001323 197.677982,159.049528 C204.827153,120.027728 184.496324,118.997566 188.045232,104.801934 C202.287755,83.6528313 204.827153,56.662651 190.073179,40.4146034 C153.570125,2.39058926 144.444362,48.0194062 114.025151,20.1351292 C101.727304,7.83728238 83.0989526,-6.73517414 56.2286492,3.40456297 C18.2385839,17.7404371 27.3303985,47.659333 27.3303985,69.3128542 C27.3303985,76.9370868 3.11408002,91.2696589 0.460095291,110.885776 C-2.88628833,135.619549 12.6277797,168.175291 27.3303985,179.329002 C50.6517938,205.692318 86.6478605,199.608476 102.87144,190.482713 C116.802125,182.646702 128.220783,192.003673 139.374493,198.087515 Z'%3E%3C/path%3E%3C/svg%3E");
+  background-size: 150px, 400px; 
+  background-position: bottom 35% right 20%, bottom left 25%; 
+  background-repeat: no-repeat; 
+  img{
+    position: relative;
+    left: 5%;
+  }
 }
 .figure-bottom {
   position: relative;
-  overflow: visible; /* Consente all'elemento di sbordare */
+  overflow: visible; 
   background-image: url('../../../assets/images/maxcoach-shape-01.png'), url('../../../assets/images/maxcoach-shape-07.png'), url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' width='200px' height='202px' fill='%23ece8e4' viewBox='0 0 200 202'%3E%3Cpath class='elementor-shape-fill' d='M139.374493,198.087515 C155.702187,206.99353 190.724846,197.001323 197.677982,159.049528 C204.827153,120.027728 184.496324,118.997566 188.045232,104.801934 C202.287755,83.6528313 204.827153,56.662651 190.073179,40.4146034 C153.570125,2.39058926 144.444362,48.0194062 114.025151,20.1351292 C101.727304,7.83728238 83.0989526,-6.73517414 56.2286492,3.40456297 C18.2385839,17.7404371 27.3303985,47.659333 27.3303985,69.3128542 C27.3303985,76.9370868 3.11408002,91.2696589 0.460095291,110.885776 C-2.88628833,135.619549 12.6277797,168.175291 27.3303985,179.329002 C50.6517938,205.692318 86.6478605,199.608476 102.87144,190.482713 C116.802125,182.646702 128.220783,192.003673 139.374493,198.087515 Z'%3E%3C/path%3E%3C/svg%3E");
-  background-size: 15%, 30%, 80%; /* Imposta le dimensioni dell'immagine di sfondo */
-  background-position: top 40% left , bottom 20% right 5%, bottom left 20%; /* Imposta la posizione dell'immagine di sfondo */
-  background-repeat: no-repeat; /* Impedisce la ripetizione delle immagini di sfondo */
+  background-size: 15%, 30%, contain; 
+  background-position: top 35% left , bottom 20% right 15%, bottom left 20%; 
+  background-repeat: no-repeat;
   img{
     position: relative;
     left: 5%;
