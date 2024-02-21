8ff8c7c15edb2114c2b6e59451831b92e154af8f
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-21 02:07:36 +0100

    feat: carousel section (to complete) End-of-day project backup

--- src/components/AppMain/AppMainSection/SectionSeven.vue
+++ src/components/AppMain/AppMainSection/SectionSeven.vue
@@ -1,8 +1,42 @@
 
 <template>
-    <div>
+
+  <div class="container-fluid">
+
+    <div class="section-container py-5">
+
+      <!-- Subtitle -->
+      <h5 class="section-subtitle text-center">
+        people are praising maxcoach
+      </h5>
+
+      <!-- Title -->
+      <h1 class="section-title text-center">
+        What make they <span>love us?</span> 
+      </h1>
+
+      <!-- carousel -->
+      <div>
+
+      </div>
+
+      <!-- Get started now -->
+      <div class="get-started d-flex flex-column align-items-center mt-5">
+        <h4 class="section-title text-center my-1">
+          Start today for getting <span>Online Certification</span> 
+        </h4>
+        <h2 class="section-title text-center">
+          You can be your own guiding star with our help!
+        </h2>
+        <button class="btn btn-green col-3 my-4">
+          Get started now
+        </button>
+      </div>
 
     </div>
+
+  </div>
+
 </template>
 
 <script >
@@ -19,5 +53,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+*{
+  border: 1px solid black;
+}
+.container-fluid{
+  background-color: #f8f8f8;
+}
+.section-title{
+  text-transform: none;
+}
+.get-started{
 
+}
+.btn-green {
+  color: white;
+  font-weight: bold;
+  font-size: small;
+  background-color: #20ad96;
+}
+.get-started {
+  position: relative;
+  overflow: visible; /* Consente all'elemento di sbordare */
+  background-image: url('../../../assets/images/maxcoach-shape-01.png'), url('../../../assets/images/maxcoach-shape-02.png');
+  background-size: 12%, 10%; /* Imposta le dimensioni dell'immagine di sfondo */
+  background-position: bottom 40% left 10px, bottom 10px right 10px; /* Imposta la posizione dell'immagine di sfondo */
+  background-repeat: no-repeat; /* Impedisce la ripetizione delle immagini di sfondo */
+}
 </style>
--- src/components/AppMain/AppMainSection/SectionSix.vue
+++ src/components/AppMain/AppMainSection/SectionSix.vue
@@ -12,50 +12,52 @@ export default {
 </script>
 
 <template>
-  <div class="overlay-effect-container">
+  <div class="overlay-effect-container-bg">
+    <div class="overlay-effect-container">
 
-    <div class="section-container my-5 pb-4">
+      <div class="section-container mt-5 pb-4">
 
-      <!-- Subtitle -->
-      <h5 class="section-subtitle text-center">
-        get in contact now
-      </h5>
+        <!-- Subtitle -->
+        <h5 class="section-subtitle text-center">
+          get in contact now
+        </h5>
 
-      <!-- Title -->
-      <h1 class="section-title text-center">
-        upcoming <span>events</span> 
-      </h1>
+        <!-- Title -->
+        <h1 class="section-title text-center">
+          upcoming <span>events</span> 
+        </h1>
 
-      <!-- Cards -->
-      <div class="d-flex flex-wrap my-4 justify-content-around">
-        <div v-for="(event, index) in store.upcomingEvents" :key="index" class="col-6 d-flex justify-content-between">
+        <!-- Cards -->
+        <div class="d-flex flex-wrap my-4 justify-content-around">
+          <div v-for="(event, index) in store.upcomingEvents" :key="index" class="col-6 d-flex justify-content-between">
 
-            <div class="event-card rounded-3 d-flex col m-3 justify-content-between px-4 py-3">
-              
-              <div class="col-8 mt-3">
-                <span class="position"><i class="fa-solid fa-location-dot me-2" style="color: #808080;"></i>{{ event.place }}</span>
-                <h5 class="mt-3">{{ event.name }}</h5>
+              <div class="event-card rounded-3 d-flex col m-3 justify-content-between px-4 py-3">
+                
+                <div class="col-8 mt-3">
+                  <span class="position"><i class="fa-solid fa-location-dot me-2" style="color: #808080;"></i>{{ event.place }}</span>
+                  <h5 class="mt-3">{{ event.name }}</h5>
+                </div>
+
+                <div class="col-4 text-center">
+                  <h1 class="event-day">
+                    {{ event.day }}
+                  </h1>
+                  <h5 class="fw-bold pb-1">
+                    {{ event.month }}
+                  </h5>
+                  <button class="btn-ticket btn col-12">
+                    Get Ticket
+                  </button>
+                </div>
               </div>
-
-              <div class="col-4 text-center">
-                <h1 class="event-day">
-                  {{ event.day }}
-                </h1>
-                <h5 class="fw-bold pb-1">
-                  {{ event.month }}
-                </h5>
-                <button class="btn-ticket btn col-12">
-                  Get Ticket
-                </button>
-              </div>
-            </div>
+          </div>
         </div>
-      </div>
 
-      <!-- Link -->
-      <p class="all-events text-center mt-2 mb-5 pb-5">
-        Excited about our events? <a href="#">View all events &rarr;</a>
-      </p>
+        <!-- Link -->
+        <p class="all-events text-center mt-2 mb-5 pb-5">
+          Excited about our events? <a href="#">View all events &rarr;</a>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -64,8 +66,16 @@ export default {
 *{
   // border: 1px solid black;
 }
+template{
+  background-color: #F8F8F8;
+}
 .overlay-effect-container{
   border-radius: 70% 0% / 20% 0%;
+  background-color: white;
+}
+.overlay-effect-container-bg{
+  background: rgb(255,255,255);
+  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(248,248,248,1) 51%, rgba(248,248,248,1) 100%);
 }
 .event-card{
   background-color: #f8f8f8;
